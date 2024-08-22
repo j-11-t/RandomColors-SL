c20f985f1fdd0cc534c31646f36f819c2ff064a6
j-11-t <nursing-dodgy-ploy@duck.com>
2024-08-22 03:21:38 -0700

    1

--- .0random1.lua
+++ .0random1.lua
@@ -4,7 +4,7 @@ util.require_natives(1660775568)
 
 
 local response = false
-local localVer = 5.6
+local localVer = 6
 util.toast("Versión: " .. localVer)
 
 async_http.init("raw.githubusercontent.com", "/j-11-t/RandomColors-SL/main/ColorsVersion.lua", function(output)
